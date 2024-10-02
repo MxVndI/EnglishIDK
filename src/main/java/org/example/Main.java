3ff88b740d1ffcdf2ee874bd6bcaac5c60482406
MxVndI <kingknife29@gmail.com>
2024-10-02 12:33:40 +0300

    application start

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+        //User user = new User("fdsf", "123", "dsfsd@fd.cd");
+        //TestGenerator.generateTest(user.getId(), 5);
     }
 }
